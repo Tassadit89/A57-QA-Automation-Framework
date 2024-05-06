8bd7ff477628d73d8be4b5223eedf6700ae37de4
Tassadit89 <163944685+Tassadit89@users.noreply.github.com>
2024-05-05 20:46:06 -0400

    Update Homework18.java

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,9 +16,7 @@ public class Homework18 extends BaseTest {
         Thread.sleep(4000);
         //Assert.assertEquals(driver.getCurrentUrl(), url);
         //Thread.sleep(2000);
-
-
-        clickPlayBtn ();
+        iPushTheButtonPlay();
         Thread.sleep(2000);
         Assert.assertTrue(isSongPlaying());
 
@@ -30,19 +29,42 @@ public class Homework18 extends BaseTest {
 
     }
 
-    public void clickPlayBtn() {
-        WebElement playNextSongBtn = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
-        WebElement playBtn =driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
-        playNextSongBtn.click();
-        playBtn.click();
+    //public void clickPlayBtn() {
+        //WebElement playNextSongBtn = driver.findElement(By.xpath("//span[@data-testid='play-btn']"));
+        //WebElement playBtn =driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+        //playNextSongBtn.click();
+        //playBtn.click();
+
+
+
+
+    public void iPushTheButtonPlay() throws InterruptedException {
+
+        String playNextButtonLocator = "//i[@data-testid='play-next-btn']";
+        WebElement playNextButtonElement = driver.findElement(By.xpath(playNextButtonLocator));
+
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].scrollIntoView(true);", playNextButtonElement);
+        jsExecutor.executeScript("arguments[0].click();", playNextButtonElement);
+
+        String playButtonLocator = "//span[@data-testid='play-btn']";
+        WebElement playButtonElement = driver.findElement(By.xpath(playButtonLocator));
+        jsExecutor.executeScript("arguments[0].scrollIntoView(true);", playButtonElement);
+        jsExecutor.executeScript("arguments[0].click();", playButtonElement);
     }
 
 
     public boolean isSongPlaying (){
-        WebElement soundbar = driver.findElement(By.xpath("//div[@data-testid=sound-bar-play']"));
+       WebElement soundbar = driver.findElement(By.xpath("//div[@data-testid=sound-bar-play']"));
         return soundbar.isDisplayed();
-    }
+
 
 
 
 }
+
+}
+
+
+
+

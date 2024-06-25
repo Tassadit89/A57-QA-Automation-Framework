f32094ddc4e911043ce69d81741dd3dfe1bd6929
Tassadit89 <163944685+Tassadit89@users.noreply.github.com>
2024-06-25 19:18:47 -0400

    Homework-23

--- src/test/java/Homework23.java
+++ src/test/java/Homework23.java
@@ -1,5 +1,28 @@
+import Pages.HomePage;
+import Pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Homework23 extends BaseTest{
 
+    @Test
+
+public void deletePlaylist() {
+       String  playlistDeletedMsg = "Deleted playlist\"New playlist\"";
+        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
+
+        loginPage.provideEmail("tassadit.talbi@testpro.io").providePassword("Password2026@").clickSubmit();
+        homePage.clickPlaylist().clickDeleteBtn();
+        Assert.assertEquals(homePage.getDeletedPlaylistMsg(),playlistDeletedMsg);
+
+
+
+
+
+
+
+    }
 
 
 
--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -3,6 +3,7 @@ package Pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class HomePage extends BasePage{
@@ -13,10 +14,40 @@ public class HomePage extends BasePage{
 
     }
 
-    By userAvatarIcon = By.cssSelector("img.avatar");
+    //By userAvatarIcon = By.cssSelector("img.avatar");
+    @FindBy(css="img.avatar")
+    private WebElement userAvatarIcon ;
+    @FindBy(css="section#playlists > ul > li:nth-of-type(6)")
+    private WebElement newPlaylist;
+
+    @FindBy(css = ".btn-delete-playlist")
+    private WebElement deleteButton ;
+
+    @FindBy (css = "div.success.show")
+    private WebElement successNotificationMsg;
 
 
     public WebElement getAvatarIcon  (){
         return findElement(userAvatarIcon);
+    }
+
+    public HomePage clickPlaylist (){
+        newPlaylist.click();
+        return this;
+
+
+    }
+
+    public HomePage clickDeleteBtn(){
+
+        deleteButton.click();
+        return this;
+    }
+
+    public HomePage getDeletedPlaylistMsg (){
+        successNotificationMsg.getText();
+        return this;
+
+
     }
 }

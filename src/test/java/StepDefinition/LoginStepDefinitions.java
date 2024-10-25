ad7adb5426b4dc1f067a174adec739449563c7fc
Tassadit89 <163944685+Tassadit89@users.noreply.github.com>
2024-10-24 20:18:28 -0400

    Homework-27

--- src/test/java/StepDefinition/LoginStepDefinitions.java
+++ src/test/java/StepDefinition/LoginStepDefinitions.java
@@ -1,4 +1,31 @@
-package StepDefinition;
+package stepDefinition;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginStepDefinitions {
-}
+
+    WebDriver driver;
+
+    WebDriverWait wait;
+
+    @Given("I open browser")
+    public void openBrowser(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-notifications");
+        options.addArguments("--remote-allow-origins=*");
+        driver=new ChromeDriver(options);
+        wait= new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+
+
+
+
+    }

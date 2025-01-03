d292f78b3bba1341f22858f5eea1756b9928bb88
Tassadit89 <163944685+Tassadit89@users.noreply.github.com>
2025-01-02 22:54:18 -0500

    Intern-UpdateEmail

--- src/test/java/UpdateEmail.java
+++ src/test/java/UpdateEmail.java
@@ -48,7 +48,7 @@ public void updateEmailWithoutAPoint(){
 
 @Test
 
- public void updateEmailWithoutAtSymbol(){
+ public void updateEmailWithoutAtSymbol(){ //unable to find the error message webelement
 
         LoginPage loginPage = new LoginPage(getDriver());
         HomePage homePage = new HomePage(getDriver());
@@ -61,6 +61,27 @@ public void updateEmailWithoutAPoint(){
         profilePage.getUpdatedProfileMsg();
 
         Assert.assertFalse(profilePage.getUpdatedProfileNotif(),successMsg);
+
+
+
+}
+
+@Test
+
+    public void updateEmailWithoutADomain (){
+
+    LoginPage loginPage = new LoginPage(getDriver());
+    HomePage homePage = new HomePage(getDriver());
+    ProfilePage profilePage = new ProfilePage(getDriver());
+    String successMsg = "Profile Updated.";
+
+    loginPage.provideEmail("tassadit.test@testpro.io").providePassword("Passwordtest2025@").clickSubmit();
+    homePage.clickOnViewProfile();
+    profilePage.provideCurrentPassword("Passwordtest2025@").provideNewEmail("tassadit.adamtestpro.io").clickSaveBtn();
+    profilePage.getUpdatedProfileMsg();
+
+    Assert.assertFalse(profilePage.getUpdatedProfileNotif(),successMsg);
+
 
 
 
